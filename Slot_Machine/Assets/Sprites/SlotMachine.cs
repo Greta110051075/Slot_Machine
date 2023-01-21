486841c9cffb9da58eb841d6a216f2201d9b1a05
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-01-21 23:47:05 +0800

    1_21

---
+++ Slot_Machine/Assets/Sprites/SlotMachine.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace greta
+{
+    public class SlotMachine : MonoBehaviour
+    {
+        public Image[] img;
+        private int a, b, c, d, e;
+        private int count;
+        private int index;
+        private bool roll;
+
+        private void Start()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            e = 0;
+        }
+
+    }
+
+}
