8dea5764688a4e2d0ea51765ed70d17d99f02dfc
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-01-22 12:20:32 +0800

    1_22

--- Slot_Machine/Assets/Sprites/SlotMachine.cs
+++ Slot_Machine/Assets/Sprites/SlotMachine.cs
@@ -5,19 +5,70 @@ namespace greta
 {
     public class SlotMachine : MonoBehaviour
     {
-        public Image[] img;
+        public Text[] txts;
         private int a, b, c, d, e;
         private int count;
         private int index;
         private bool roll;
 
+        public Image[] i = new Image[5];
+        public int[] num = new int[5];
+
         private void Start()
         {
-            a = 0;
-            b = 0;
-            c = 0;
-            d = 0;
-            e = 0;
+            
+
+            foreach (Text item in txts) 
+            {
+                item.text = "0";
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (count == 0 && !roll)
+                {
+                    roll = true;
+                }
+                else
+                {
+                    count++;
+                    if (count == 3)
+                    {
+                        if(int.Parse(txts[0].text) == 7 && int.Parse(txts[1].text) == 7 && int.Parse(txts[2].text) ==7)
+                        {
+                            Debug.Log("Congratulation");
+                        }
+                    }
+
+                    if (count == 4)
+                    {
+                        count = 0;
+                    }
+
+                }
+
+            }
+
+            if (roll)
+            {
+                for (int j = count; j < txts.Length; j++)
+                {
+                    txts[j].text = index + "";
+                }
+
+                if (Time.frameCount % 6 == 0)
+                {
+                    index++;
+
+                    if (index > 9)
+                    {
+                        index = 0;
+                    }
+                }
+            }
         }
 
     }
